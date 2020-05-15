0c2edec70308af18788ffc885df5bead7a69df99
Valeryia Strom <vstrom@its.jnj.com>
2020-05-15 21:48:20 +0300

    add geckodriver

--- node-chrome.bat
+++ node-chrome.bat
@@ -1,1 +1,1 @@
-java -jar selenium-server-standalone-3.141.59.jar -role 	node -hub 	http://192.168.56.1:4444/grid/register 
+java -Dwebdriver.chrome.driver=\drivers\chromedriver.exe -jar selenium-server-standalone-3.141.59.jar -role 	node -hub 	http://192.168.56.1:4444/grid/register 
--- node-firefox.bat
+++ node-firefox.bat
@@ -1,1 +1,1 @@
-java -Dwebdriver.chrome.driver=\chromedriver\chromedriver.exe -jar selenium-server-standalone-3.141.59.jar -role 	node -hub 	http://192.168.56.1:4444/grid/register 
+java -Dwebdriver.chrome.driver=\drivers\geckodriver.exe -jar selenium-server-standalone-3.141.59.jar -role 	node -hub 	http://192.168.56.1:4444/grid/register 
